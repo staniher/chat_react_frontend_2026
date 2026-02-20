04b7a16432385257035e065ae1bc64f60c5f5f4d
staniher <80840927+staniher@users.noreply.github.com>
2026-02-20 16:33:59 +0200

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 
+// Ajout de /api ici
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: `${API_URL}/api`,
 });
 
 export default api;
